093d734a1d5d9ff624902bb7e43459c31be7bae4
Pragati Gupta <pragatig746@gmail.com>
2022-04-01 20:46:20 +0530

    search functionality

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React,{useState} from 'react'
 
 const Home = () => {
+
   // const repos = async () => {
   //   const url = `https://api.github.com/users/Shikha291/repos`;
 
@@ -16,9 +17,21 @@ const Home = () => {
   //   let parseData1 = await data1.json();
   //   console.log(parseData1);
   // }
-
+  const [Tech, setTech] = useState("");
+  const handleChange = (e) =>{
+    setTech(e.target.value);
+  }
+  const handleSubmit = (e) =>{
+    console.log(Tech);
+  }
   return (
-    <div></div>
+    <div>
+      <form onSubmit={handleSubmit} className="form-inline d-flex justify-content-center md-form form-sm mt-0">
+        <i className="fas fa-search" aria-hidden="true"></i>
+        <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search"aria-label="Search" name="tech"
+          value={Tech} onChange={handleChange}/>
+    </form>
+    </div>
     // <div>
     //     <button onClick={updateState()}>Github repos</button>
     // </div>
